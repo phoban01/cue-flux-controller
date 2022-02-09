9cf2412e59fb93f1686fc622709d456376232f32
Piaras Hoban <phoban01@gmail.com>
2022-02-09 16:32:32 +0000

    ctrl: add housekeeping reconciler methods
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

--- controllers/cueinstance_controller.go
+++ controllers/cueinstance_controller.go
@@ -17,14 +17,42 @@ limitations under the License.
 package controllers
 
 import (
+	"bytes"
 	"context"
+	"crypto/sha1"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"sort"
+	"strings"
+	"time"
 
+	"github.com/fluxcd/pkg/apis/meta"
+	"github.com/fluxcd/pkg/runtime/acl"
+	"github.com/fluxcd/pkg/runtime/events"
+	"github.com/fluxcd/pkg/ssa"
+	"github.com/fluxcd/pkg/untar"
+	"github.com/hashicorp/go-retryablehttp"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/reference"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/tools/reference"
+
 	cuev1alpha1 "cue-flux-controller.git/api/v1alpha1"
+	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
+	apimeta "k8s.io/apimachinery/pkg/api/meta"
 )
 
 // CueInstanceReconciler reconciles a CueInstance object
@@ -37,15 +65,14 @@ type CueInstanceReconciler struct {
 //+kubebuilder:rbac:groups=cue.contrib.flux.io,resources=cueinstances/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=cue.contrib.flux.io,resources=cueinstances/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the CueInstance object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
+// SetupWithManager sets up the controller with the Manager.
+func (r *CueInstanceReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&cuev1alpha1.CueInstance{}).
+		Complete(r)
+}
+
+// Reconcile is the main reconciler loop.
 func (r *CueInstanceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
@@ -54,9 +81,360 @@ func (r *CueInstanceReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	return ctrl.Result{}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
-func (r *CueInstanceReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&cuev1alpha1.CueInstance{}).
-		Complete(r)
+func (r *CueInstanceReconciler) reconcile(ctx context.Context, obj *cuev1alpha1.CueInstance) error {
+	return nil
+}
+
+func (r *CueInstanceReconciler) build(ctx context.Context, dir string, tags map[string]string) ([]byte, error) {
+	return nil, nil
+}
+
+func (r *CueInstanceReconciler) apply(ctx context.Context, manager *ssa.ResourceManager, cueInstance cuev1alpha1.CueInstance, revision string, objects []*unstructured.Unstructured) (bool, *ssa.ChangeSet, error) {
+	log := ctrl.LoggerFrom(ctx)
+
+	if err := ssa.SetNativeKindsDefaults(objects); err != nil {
+		return false, nil, err
+	}
+
+	applyOpts := ssa.DefaultApplyOptions()
+	applyOpts.Exclusions = map[string]string{
+		fmt.Sprintf("%s/reconcile", cuev1alpha1.GroupVersion.Group): cuev1alpha1.DisabledValue,
+	}
+
+	// contains only CRDs and Namespaces
+	var stageOne []*unstructured.Unstructured
+
+	// contains all objects except for CRDs and Namespaces
+	var stageTwo []*unstructured.Unstructured
+
+	// contains the objects' metadata after apply
+	resultSet := ssa.NewChangeSet()
+
+	for _, u := range objects {
+		if ssa.IsClusterDefinition(u) {
+			stageOne = append(stageOne, u)
+		} else {
+			stageTwo = append(stageTwo, u)
+		}
+	}
+
+	var changeSetLog strings.Builder
+
+	// validate, apply and wait for CRDs and Namespaces to register
+	if len(stageOne) > 0 {
+		changeSet, err := manager.ApplyAll(ctx, stageOne, applyOpts)
+		if err != nil {
+			return false, nil, err
+		}
+		resultSet.Append(changeSet.Entries)
+
+		if changeSet != nil && len(changeSet.Entries) > 0 {
+			log.Info("server-side apply completed", "output", changeSet.ToMap())
+			for _, change := range changeSet.Entries {
+				if change.Action != string(ssa.UnchangedAction) {
+					changeSetLog.WriteString(change.String() + "\n")
+				}
+			}
+		}
+
+		if err := manager.Wait(stageOne, ssa.WaitOptions{
+			Interval: 2 * time.Second,
+			Timeout:  cueInstance.GetTimeout(),
+		}); err != nil {
+			return false, nil, err
+		}
+	}
+
+	// sort by kind, validate and apply all the others objects
+	sort.Sort(ssa.SortableUnstructureds(stageTwo))
+	if len(stageTwo) > 0 {
+		changeSet, err := manager.ApplyAll(ctx, stageTwo, applyOpts)
+		if err != nil {
+			return false, nil, fmt.Errorf("%w\n%s", err, changeSetLog.String())
+		}
+		resultSet.Append(changeSet.Entries)
+
+		if changeSet != nil && len(changeSet.Entries) > 0 {
+			log.Info("server-side apply completed", "output", changeSet.ToMap())
+			for _, change := range changeSet.Entries {
+				if change.Action != string(ssa.UnchangedAction) {
+					changeSetLog.WriteString(change.String() + "\n")
+				}
+			}
+		}
+	}
+
+	// emit event only if the server-side apply resulted in changes
+	applyLog := strings.TrimSuffix(changeSetLog.String(), "\n")
+	if applyLog != "" {
+		r.event(ctx, cueInstance, revision, events.EventSeverityInfo, applyLog, nil)
+	}
+
+	return applyLog != "", resultSet, nil
+}
+
+func (r *CueInstanceReconciler) checkDependencies(source sourcev1.Source, cueInstance cuev1alpha1.CueInstance) error {
+	for _, d := range cueInstance.Spec.DependsOn {
+		if d.Namespace == "" {
+			d.Namespace = cueInstance.GetNamespace()
+		}
+		dName := types.NamespacedName(d)
+		var k cuev1alpha1.CueInstance
+		err := r.Get(context.Background(), dName, &k)
+		if err != nil {
+			return fmt.Errorf("unable to get '%s' dependency: %w", dName, err)
+		}
+
+		if len(k.Status.Conditions) == 0 || k.Generation != k.Status.ObservedGeneration {
+			return fmt.Errorf("dependency '%s' is not ready", dName)
+		}
+
+		if !apimeta.IsStatusConditionTrue(k.Status.Conditions, meta.ReadyCondition) {
+			return fmt.Errorf("dependency '%s' is not ready", dName)
+		}
+
+		if k.Spec.SourceRef.Name == cueInstance.Spec.SourceRef.Name && k.Spec.SourceRef.Namespace == cueInstance.Spec.SourceRef.Namespace && k.Spec.SourceRef.Kind == cueInstance.Spec.SourceRef.Kind && source.GetArtifact().Revision != k.Status.LastAppliedRevision {
+			return fmt.Errorf("dependency '%s' is not updated yet", dName)
+		}
+	}
+
+	return nil
+}
+
+func (r *CueInstanceReconciler) download(artifact *sourcev1.Artifact, tmpDir string) error {
+	artifactURL := artifact.URL
+	if hostname := os.Getenv("SOURCE_CONTROLLER_LOCALHOST"); hostname != "" {
+		u, err := url.Parse(artifactURL)
+		if err != nil {
+			return err
+		}
+		u.Host = hostname
+		artifactURL = u.String()
+	}
+
+	req, err := retryablehttp.NewRequest(http.MethodGet, artifactURL, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create a new request: %w", err)
+	}
+
+	resp, err := r.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to download artifact, error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// check response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to download artifact from %s, status: %s", artifactURL, resp.Status)
+	}
+
+	var buf bytes.Buffer
+
+	// verify checksum matches origin
+	if err := r.verifyArtifact(artifact, &buf, resp.Body); err != nil {
+		return err
+	}
+
+	// extract
+	if _, err = untar.Untar(&buf, tmpDir); err != nil {
+		return fmt.Errorf("failed to untar artifact, error: %w", err)
+	}
+
+	return nil
+}
+
+func (r *CueInstanceReconciler) verifyArtifact(artifact *sourcev1.Artifact, buf *bytes.Buffer, reader io.Reader) error {
+	hasher := sha256.New()
+
+	// for backwards compatibility with source-controller v0.17.2 and older
+	if len(artifact.Checksum) == 40 {
+		hasher = sha1.New()
+	}
+
+	// compute checksum
+	mw := io.MultiWriter(hasher, buf)
+	if _, err := io.Copy(mw, reader); err != nil {
+		return err
+	}
+
+	if checksum := fmt.Sprintf("%x", hasher.Sum(nil)); checksum != artifact.Checksum {
+		return fmt.Errorf("failed to verify artifact: computed checksum '%s' doesn't match advertised '%s'",
+			checksum, artifact.Checksum)
+	}
+
+	return nil
+}
+
+func (r *CueInstanceReconciler) prune(ctx context.Context, manager *ssa.ResourceManager, cueInstance cuev1alpha1.CueInstance, revision string, objects []*unstructured.Unstructured) (bool, error) {
+	if !cueInstance.Spec.Prune {
+		return false, nil
+	}
+
+	log := ctrl.LoggerFrom(ctx)
+
+	opts := ssa.DeleteOptions{
+		PropagationPolicy: metav1.DeletePropagationBackground,
+		Inclusions:        manager.GetOwnerLabels(cueInstance.Name, cueInstance.Namespace),
+		Exclusions: map[string]string{
+			fmt.Sprintf("%s/prune", cuev1alpha1.GroupVersion.Group):     cuev1alpha1.DisabledValue,
+			fmt.Sprintf("%s/reconcile", cuev1alpha1.GroupVersion.Group): cuev1alpha1.DisabledValue,
+		},
+	}
+
+	changeSet, err := manager.DeleteAll(ctx, objects, opts)
+	if err != nil {
+		return false, err
+	}
+
+	// emit event only if the prune operation resulted in changes
+	if changeSet != nil && len(changeSet.Entries) > 0 {
+		log.Info(fmt.Sprintf("garbage collection completed: %s", changeSet.String()))
+		r.event(ctx, cueInstance, revision, events.EventSeverityInfo, changeSet.String(), nil)
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (r *CueInstanceReconciler) getSource(ctx context.Context, cueInstance cuev1alpha1.CueInstance) (sourcev1.Source, error) {
+	var source sourcev1.Source
+	sourceNamespace := cueInstance.GetNamespace()
+	if cueInstance.Spec.SourceRef.Namespace != "" {
+		sourceNamespace = cueInstance.Spec.SourceRef.Namespace
+	}
+
+	namespacedName := types.NamespacedName{
+		Namespace: sourceNamespace,
+		Name:      cueInstance.Spec.SourceRef.Name,
+	}
+
+	if r.NoCrossNamespaceRefs && sourceNamespace != cueInstance.GetNamespace() {
+		return source, acl.AccessDeniedError(
+			fmt.Sprintf("can't access '%s/%s', cross-namespace references have been blocked",
+				cueInstance.Spec.SourceRef.Kind, namespacedName))
+	}
+
+	switch cueInstance.Spec.SourceRef.Kind {
+	case sourcev1.GitRepositoryKind:
+		var repository sourcev1.GitRepository
+		err := r.Client.Get(ctx, namespacedName, &repository)
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				return source, err
+			}
+			return source, fmt.Errorf("unable to get source '%s': %w", namespacedName, err)
+		}
+		source = &repository
+	default:
+		return source, fmt.Errorf("source `%s` kind '%s' not supported",
+			cueInstance.Spec.SourceRef.Name, cueInstance.Spec.SourceRef.Kind)
+	}
+	return source, nil
+}
+
+func (r *CueInstanceReconciler) finalize(ctx context.Context, cueInstance cuev1alpha1.CueInstance) (ctrl.Result, error) {
+	// Record deleted status
+	r.recordReadiness(ctx, cueInstance)
+
+	// Remove our finalizer from the list and update it
+	controllerutil.RemoveFinalizer(&cueInstance, cuev1alpha1.CueInstanceFinalizer)
+	if err := r.Update(ctx, &cueInstance); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Stop reconciliation as the object is being deleted
+	return ctrl.Result{}, nil
+}
+
+func (r *CueInstanceReconciler) event(ctx context.Context, cueInstance cuev1alpha1.CueInstance, revision, severity, msg string, metadata map[string]string) {
+	log := ctrl.LoggerFrom(ctx)
+
+	if r.EventRecorder != nil {
+		annotations := map[string]string{
+			cuev1alpha1.GroupVersion.Group + "/revision": revision,
+		}
+
+		eventtype := "Normal"
+		if severity == events.EventSeverityError {
+			eventtype = "Warning"
+		}
+
+		r.EventRecorder.AnnotatedEventf(&cueInstance, annotations, eventtype, severity, msg)
+	}
+
+	if r.ExternalEventRecorder != nil {
+		objRef, err := reference.GetReference(r.Scheme, &cueInstance)
+		if err != nil {
+			log.Error(err, "unable to send event")
+			return
+		}
+		if metadata == nil {
+			metadata = map[string]string{}
+		}
+		if revision != "" {
+			metadata["revision"] = revision
+		}
+
+		reason := severity
+		if c := apimeta.FindStatusCondition(cueInstance.Status.Conditions, meta.ReadyCondition); c != nil {
+			reason = c.Reason
+		}
+
+		if err := r.ExternalEventRecorder.Eventf(*objRef, metadata, severity, reason, msg); err != nil {
+			log.Error(err, "unable to send event")
+			return
+		}
+	}
+}
+
+func (r *CueInstanceReconciler) recordReadiness(ctx context.Context, cueInstance cuev1alpha1.CueInstance) {
+	if r.MetricsRecorder == nil {
+		return
+	}
+	log := ctrl.LoggerFrom(ctx)
+
+	objRef, err := reference.GetReference(r.Scheme, &cueInstance)
+	if err != nil {
+		log.Error(err, "unable to record readiness metric")
+		return
+	}
+	if rc := apimeta.FindStatusCondition(cueInstance.Status.Conditions, meta.ReadyCondition); rc != nil {
+		r.MetricsRecorder.RecordCondition(*objRef, *rc, !cueInstance.DeletionTimestamp.IsZero())
+	} else {
+		r.MetricsRecorder.RecordCondition(*objRef, metav1.Condition{
+			Type:   meta.ReadyCondition,
+			Status: metav1.ConditionUnknown,
+		}, !cueInstance.DeletionTimestamp.IsZero())
+	}
+}
+
+func (r *CueInstanceReconciler) recordSuspension(ctx context.Context, cueInstance cuev1alpha1.CueInstance) {
+	if r.MetricsRecorder == nil {
+		return
+	}
+	log := ctrl.LoggerFrom(ctx)
+
+	objRef, err := reference.GetReference(r.Scheme, &cueInstance)
+	if err != nil {
+		log.Error(err, "unable to record suspended metric")
+		return
+	}
+
+	if !cueInstance.DeletionTimestamp.IsZero() {
+		r.MetricsRecorder.RecordSuspend(*objRef, false)
+	} else {
+		r.MetricsRecorder.RecordSuspend(*objRef, cueInstance.Spec.Suspend)
+	}
+}
+
+func (r *CueInstanceReconciler) patchStatus(ctx context.Context, req ctrl.Request, newStatus cuev1alpha1.CueInstanceStatus) error {
+	var cueInstance cuev1alpha1.CueInstance
+	if err := r.Get(ctx, req.NamespacedName, &cueInstance); err != nil {
+		return err
+	}
+
+	patch := client.MergeFrom(cueInstance.DeepCopy())
+	cueInstance.Status = newStatus
+
+	return r.Status().Patch(ctx, &cueInstance, patch)
 }
--- go.mod
+++ go.mod
@@ -4,53 +4,87 @@ go 1.17
 
 require (
 	github.com/fluxcd/pkg/apis/meta v0.10.2
+	github.com/fluxcd/pkg/runtime v0.12.4
+	github.com/fluxcd/pkg/ssa v0.13.0
+	github.com/fluxcd/pkg/untar v0.1.0
+	github.com/fluxcd/source-controller/api v0.21.2
+	github.com/hashicorp/go-retryablehttp v0.7.0
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.17.0
-	k8s.io/apimachinery v0.23.0
+	k8s.io/apimachinery v0.23.1
 	k8s.io/client-go v0.23.0
 	sigs.k8s.io/controller-runtime v0.11.0
 )
 
 require (
 	cloud.google.com/go v0.81.0 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
 	github.com/Azure/go-autorest/autorest v0.11.18 // indirect
 	github.com/Azure/go-autorest/autorest/adal v0.9.13 // indirect
 	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
 	github.com/Azure/go-autorest/logger v0.2.1 // indirect
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/MakeNowJust/heredoc v0.0.0-20170808103936-bb23615498cd // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
+	github.com/exponent-io/jsonpath v0.0.0-20151013193312-d6023ce2651d // indirect
+	github.com/fluxcd/pkg/apis/acl v0.0.3 // indirect
 	github.com/form3tech-oss/jwt-go v3.2.3+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
-	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/go-errors/errors v1.0.1 // indirect
+	github.com/go-logr/logr v1.2.2 // indirect
 	github.com/go-logr/zapr v1.2.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.5 // indirect
+	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.5 // indirect
-	github.com/google/gofuzz v1.1.0 // indirect
-	github.com/google/uuid v1.1.2 // indirect
+	github.com/google/btree v1.0.1 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
+	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
+	github.com/moby/spdystream v0.2.0 // indirect
+	github.com/moby/term v0.0.0-20210610120745-9d4ed1856297 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.11.0 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.28.0 // indirect
 	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/russross/blackfriday v1.5.2 // indirect
+	github.com/spf13/cobra v1.2.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/xlab/treeprint v0.0.0-20181112141820-a009c3971eca // indirect
+	go.starlark.net v0.0.0-20200306205701-8dd3e2ee1dd5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.19.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
+	golang.org/x/net v0.0.0-20211215060638-4ddde0e984e9 // indirect
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
 	golang.org/x/sys v0.0.0-20211029165221-6e7872819dc8 // indirect
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
@@ -63,13 +97,18 @@ require (
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-	k8s.io/api v0.23.0 // indirect
+	k8s.io/api v0.23.1 // indirect
 	k8s.io/apiextensions-apiserver v0.23.0 // indirect
+	k8s.io/cli-runtime v0.23.0 // indirect
 	k8s.io/component-base v0.23.0 // indirect
 	k8s.io/klog/v2 v2.30.0 // indirect
 	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65 // indirect
-	k8s.io/utils v0.0.0-20210930125809-cb0fa318a74b // indirect
-	sigs.k8s.io/json v0.0.0-20211020170558-c049b76a60c6 // indirect
+	k8s.io/kubectl v0.22.2 // indirect
+	k8s.io/utils v0.0.0-20211208161948-7d6a63dca704 // indirect
+	sigs.k8s.io/cli-utils v0.27.0 // indirect
+	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
+	sigs.k8s.io/kustomize/api v0.10.1 // indirect
+	sigs.k8s.io/kustomize/kyaml v0.13.0 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.0 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
